599470d917f5b96ae522dfea0b6e41b2a41e7eb1
plucafo <plucaf87@gmail.com>
2024-03-12 15:54:45 -0700

    added server.js file and created connection to database

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const mysql = require('mysql2');
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+// Express middleware
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+// Create connection to mysql database
+const db = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "", // Empty string for no password
+    database: "employees_db"
+  });
+
+app.listen(PORT, () => {
+    console.log(`Listening on port ${PORT}`);
+})
