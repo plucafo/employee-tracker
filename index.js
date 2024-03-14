7e7a427b221a1a6fc269c6ba7c757b56a79a5efd
plucafo <plucaf87@gmail.com>
2024-03-14 11:25:07 -0700

    added switch statement for users initial prompt choices

--- index.js
+++ index.js
@@ -34,22 +34,17 @@ function promptUser() {
     .prompt(questions)
     .then((answers) => {
       // Do stuff with answers here
-      db.query(
-        `SELECT e1.id, e1.first_name, e1.last_name, e1.title,
-         r.title AS role, e1.salary,
-         CONCAT(e2.first_name, ' ', e2.last_name) AS manager
-         FROM employees e1
-         LEFT JOIN employees e2 ON e1.manager_id = e2.id
-         LEFT JOIN roles r ON e1.roles_id = r.id;`,
-        (err, rows) => {
-          if (err) {
-            console.error("Error querying database:", err);
-            return;
-          }
-          console.table(rows);
+      switch (answers.start) {
+        case "View All Employees":
+          viewAllEmployees();
+          break;
+        case "Add Employee":
+          console.log('TODO: CREATE FUNCTION TO ADD EMPLOYEE');
           process.exit();
-        }
-      ); // log the table for testing
+          break;
+        default:
+          console.log("Invalid selection");
+      }
     })
     .catch((error) => {
       console.error("Something went wrong:", error);
@@ -57,3 +52,23 @@ function promptUser() {
 }
 
 // promptUser();
+
+// FUNCTIONS TO BE MOVED TO DIFFERENT JS FILE LATER
+function viewAllEmployees() {
+  db.query(
+    `SELECT e1.id, e1.first_name, e1.last_name, e1.title,
+     r.title AS role, e1.salary,
+     CONCAT(e2.first_name, ' ', e2.last_name) AS manager
+     FROM employees e1
+     LEFT JOIN employees e2 ON e1.manager_id = e2.id
+     LEFT JOIN roles r ON e1.roles_id = r.id;`,
+    (err, rows) => {
+      if (err) {
+        console.error("Error querying database:", err);
+        return;
+      }
+      console.table(rows);
+      process.exit();
+    }
+  );
+}
