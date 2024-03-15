787cb4d5b808dcadf02b1ae02f964723d0d47902
plucafo <plucaf87@gmail.com>
2024-03-14 18:05:25 -0700

    added database connection code to the db.js file

--- db.js
+++ db.js
@@ -1,17 +1,19 @@
-// const db = mysql.createConnection({
-//     host: "localhost",
-//     user: "root",
-//     password: "",
-//     database: "employees_db",
-//   });
-  
-//   // Establish database connection
-//   db.connect((err) => {
-//     if (err) {
-//       console.error("Error connecting to database:", err);
-//       return;
-//     }
-//     console.log("Connected to the employees_db database");
-//     // Start the application after connecting to the database
-//     promptUser();
-//   });
+const mysql = require("mysql2");
+
+const db = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "",
+  database: "employees_db",
+});
+
+// Establish database connection
+db.connect((err) => {
+  if (err) {
+    console.error("Error connecting to database:", err);
+    return;
+  }
+  console.log("Connected to the employees_db database");
+});
+
+module.exports = db;
--- index.js
+++ index.js
@@ -1,23 +1,5 @@
 const inquirer = require("inquirer");
-const mysql = require("mysql2");
-
-const db = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "",
-  database: "employees_db",
-});
-
-// Establish database connection
-db.connect((err) => {
-  if (err) {
-    console.error("Error connecting to database:", err);
-    return;
-  }
-  console.log("Connected to the employees_db database");
-  // Start the application after connecting to the database
-  promptUser();
-});
+const db = require('./db');
 
 const questions = [
   {
@@ -39,7 +21,7 @@ function promptUser() {
           viewAllEmployees();
           break;
         case "Add Employee":
-          console.log('TODO: CREATE FUNCTION TO ADD EMPLOYEE');
+          console.log("TODO: CREATE FUNCTION TO ADD EMPLOYEE");
           process.exit();
           break;
         default:
@@ -51,17 +33,15 @@ function promptUser() {
     });
 }
 
-// promptUser();
-
 // FUNCTIONS TO BE MOVED TO DIFFERENT JS FILE LATER
 function viewAllEmployees() {
   db.query(
     `SELECT e1.id, e1.first_name, e1.last_name, e1.title,
-     r.title AS role, e1.salary,
-     CONCAT(e2.first_name, ' ', e2.last_name) AS manager
-     FROM employees e1
-     LEFT JOIN employees e2 ON e1.manager_id = e2.id
-     LEFT JOIN roles r ON e1.roles_id = r.id;`,
+    r.title AS role, e1.salary,
+    CONCAT(e2.first_name, ' ', e2.last_name) AS manager
+    FROM employees e1
+    LEFT JOIN employees e2 ON e1.manager_id = e2.id
+    LEFT JOIN roles r ON e1.roles_id = r.id;`,
     (err, rows) => {
       if (err) {
         console.error("Error querying database:", err);
@@ -72,3 +52,5 @@ function viewAllEmployees() {
     }
   );
 }
+
+promptUser();
